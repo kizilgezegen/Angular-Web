dccc2955c700a2254fb26878257b8256296564af
HBezgin <handan.bezgin@netix.com.tr>
2019-09-04 17:18:13 +0300

    Base components added

--- angular-web/src/app/app.module.ts
+++ angular-web/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductComponent } from './product/product.component';
+import { NavComponent } from './nav/nav.component';
+import { CategoryComponent } from './category/category.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductComponent,
+    NavComponent,
+    CategoryComponent
   ],
   imports: [
     BrowserModule,
