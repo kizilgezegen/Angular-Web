3ac299f456af28e280ac5018af18c24cc334d8ed
HBezgin <handan.bezgin@netix.com.tr>
2019-09-04 17:58:44 +0300

    product list added

--- angular-web/src/app/product/product.component.ts
+++ angular-web/src/app/product/product.component.ts
@@ -9,6 +9,24 @@ export class ProductComponent implements OnInit {
 
   constructor() { }
 
+  title="Ürün Listesi";
+  products :any[] = [
+    {
+      id: 1,
+      name: "Laptop",
+      price: 1223342,
+      categoryId: 1,
+      description:"Asus Zen Book"
+    },
+    {
+      id: 2,
+      name: "Mouse",
+      price: 12233,
+      categoryId: 2,
+      description:"Logitech"
+    }
+  ];
+
   ngOnInit() {
   }
 
